238eac8ed53fb61b164bb88d658e11054585a80f
Shotaro Hamada <hamadashotaro1212@gmail.com>
2025-07-28 23:37:00 -0600

    Fixed to use the frontend url

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+console.log('DEBUG: VITE_API_URL is:', import.meta.env.VITE_API_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <App />
